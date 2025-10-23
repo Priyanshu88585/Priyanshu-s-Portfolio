02c7ad64fcf8f48b45c2aea72d1ebf9eaff7a514
PRIYANSHU KESHARWANI <134344291+Priyanshu88585@users.noreply.github.com>
2025-10-23 08:39:44 +0530

    Update profile image source in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,7 +67,7 @@ const Hero = () => {
       <div className="hero-right">
         <div className="profile-photo-wrap card">
           <img
-            src= "https://media.licdn.com/dms/image/v2/D5603AQHwEr2IAwFKjA/profile-displayphoto-scale_200_200/B56ZjLcG9WHUAY-/0/1755759802549?e=1760572800&v=beta&t=_FgUqvwiFQwEsUTaVOodx4Q0Ei9jrAvKR5sfXCDF4Ic"
+            src= "https://instagram.fjlr3-1.fna.fbcdn.net/v/t51.2885-19/566109011_18290561581281912_6637570721334868658_n.jpg?stp=dst-jpg_s320x320_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.fjlr3-1.fna.fbcdn.net&_nc_cat=100&_nc_oc=Q6cZ2QG35ZuOZsms96a-WqTFOzErbAhRZhCLrBLk6okxyHHNBFb39IqcBFNndbbc-DIdAGtnxfLt4-FNx5E65QhnAat7&_nc_ohc=qrA2Jni_ABIQ7kNvwExS0AE&_nc_gid=A3WqRrj1TzkClbn4m1axnA&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfeJxH5R8o3_pCJzLlKtu_UuRfrR0_VW_dlc069Gbbl7Ig&oe=68FF8512&_nc_sid=8b3546"
             alt="Profile"
             className="profile-photo"
           />
